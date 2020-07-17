24a8158c72207e904d86ada5046aba118d321273
Grigori <jlavyan@gmail.com>
2020-07-17 19:45:34 +0400

    FIX handle double tap on map

---
+++ WeatherMap/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  WeatherMap
+//
+//  Created by Grigori on 7/17/20.
+//  Copyright © 2020 Test. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+    @IBOutlet private var aMap: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addHandlers()
+    }
+}
+
+
+private extension ViewController{
+    /// Add double tap handler on map view
+    private func addHandlers(){
+        let doubleGesture = UITapGestureRecognizer(target: self, action:#selector(onDoubleTap))
+        doubleGesture.numberOfTapsRequired = 2;
+        doubleGesture.numberOfTouchesRequired = 1;
+
+        // We using tap gesture since map kit cancel double tap
+        let tapGesture = UILongPressGestureRecognizer(target: self, action:#selector(onDoubleTap))
+        tapGesture.numberOfTapsRequired = 1;
+        tapGesture.numberOfTouchesRequired = 1;
+
+        doubleGesture.delegate = self
+        tapGesture.delegate = self
+        aMap.addGestureRecognizer(doubleGesture)
+        aMap.addGestureRecognizer(tapGesture)
+    }
+    
+}
+
+
+extension ViewController: UIGestureRecognizerDelegate{
+    @objc func onDoubleTap(gestureRecognizer: UITapGestureRecognizer){
+        
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+}
